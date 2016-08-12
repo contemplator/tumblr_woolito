778135d5f8a75db54bb85d24eca070a7891ee454
contemplator <contemplation8213@gmail.com>
2016-08-12 12:06:51 +0800

    finish index-mobile css

--- custom/js/index-mobile.js
+++ custom/js/index-mobile.js
@@ -104,7 +104,7 @@ function render_posts(posts) {
                 case "video":
                     post_html = render_video(posts[i]);
                     break;
-                case "regular":
+                case "text":
                     post_html = render_text(posts[i]);
                     break;
                 case "photo":
@@ -123,16 +123,20 @@ function render_photo(post) {
     var article = $("<div></div>");
     article.attr("id", post.id);
     article.addClass("type_photo grid-item");
-    article.attr("rel", post['url-with-slug']);
+    article.attr("rel", post['post_url']);
     var article_content = $('<div class="article-content"></div>');
     var photo_wrap = $('<div class="photo-wrap post"></div>');
     var photo_post = $('<div class="photo--post"></div>')
-    var photo_link = $('<a href="' + post['url-with-slug'] + '"></a>')
-    var photo_img = $('<img src="' + post['photo-url-400'] + '" data-highres="' + post['photo-url-1280'] + '" alt="' + post.slug + '">')
+    console.log(post);
+    for (var i = 0; i < post.photos.length; i++) {
+        var photo_link = $('<a href="' + post.post_url + ' target=\"_blank\""></a>');
+        var photo_img = $('<img src="' + post.photos[i].original_size.url + '" alt="' + post.slug + '">');
+    }
+
     photo_link.append(photo_img);
     photo_post.append(photo_link);
     photo_wrap.append(photo_post);
-    var caption = $('<div class="caption">' + post['photo-caption'] + '</div>');
+    var caption = $('<div class="caption">' + post['summary'] + '</div>');
     article_content.append(photo_wrap).append(caption);
     var icon = render_icon(post);
     article_content.append(icon);
@@ -144,18 +148,20 @@ function render_video(post) {
     var article = $("<div></div>");
     article.attr("id", post.id);
     article.addClass("type_video grid-item");
-    article.attr("rel", post['url-with-slug']);
+    article.attr("rel", post['post_url']);
     var article_content = $('<div class="article-content"></div>');
     var video_stage = $("<div class='video-stage'></div>");
     var video_container = $("<div class='video-container ready video' id='" + post.id + "'></div>");
-    var iframe = render_iframe(post);
-
+    // var iframe = render_iframe(post);
+    var shortcut = $('<div class="video-shortcut"></div>')
+    var shortcut_img = $('<img >');
+    shortcut_img.attr("src", post.thumbnail_url);
+    shortcut.append(shortcut_img);
     // var shortcut = video_shortcut(post);
     // video_container.append(shortcut);
-
-    video_container.append(iframe);
+    video_container.append(shortcut);
     video_stage.append(video_container);
-    var caption = $('<div class="caption">' + post['video-caption'] + '</div>');
+    var caption = $('<div class="caption">' + post['caption'] + '</div>');
     article_content.append(video_stage).append(caption);
     var icon = render_icon(post);
     article_content.append(icon);
@@ -167,17 +173,18 @@ function render_text(post) {
     var article = $("<div></div>");
     article.attr("id", post.id);
     article.addClass("type_text grid-item");
-    article.attr("rel", post['url-with-slug']);
+    article.attr("rel", post['post_url']);
     var article_content = $("<div></div>");
     article_content.addClass("article_content");
     var title = $("<h2></h2>");
-    var title_link = $("<a href='" + post['url-with-slug'] + "' title='" + post['regular-title'] + "'>" + post.slug + "</a>");
+    var title_link = $("<a href='" + post['post_url'] + "' target=\"_blank\" title='點擊觀看作品介紹' data-toggle='tooltip' data-placement='right' >" + post.slug + "</a>");
     title.append(title_link);
-    var body = post['regular-body'];
+    var body = post['body'];
+    body = analysis_caption_iframe(post.body, post.id);
     if (body.indexOf("<!-- more -->") > -1) {
         var start_index = body.indexOf("<!-- more -->");
         body = body.substring(0, start_index);
-        body += '<a href="' + post['url-with-slug'] + '">KEEP READING</a>';
+        body += '<a href="' + post['post_url'] + '" target=\"_blank\" class="readmore">KEEP READING</a>';
     }
     article_content.append(title).append(body);
     var icon = render_icon(post);
@@ -186,14 +193,37 @@ function render_text(post) {
     return article;
 }
 
+function analysis_caption_iframe(caption, post_id) {
+    if (!(caption.indexOf("youtube_iframe") > -1)) {
+        return caption;
+    }
+    var patt = /https:\/\/www.youtube.com\/embed\/(\w+)\?/i;
+    var patt2 = /<figure(.*)>(.*)<\/figure>/i;
+    var youtube_id = caption.match(patt)[1];
+    var link_element = $('<a target="_blank"></a>');
+    link_element.attr("href", "http://woolito.tumblr.com/post/" + post_id + "/");
+    var shortcut_element = $("<img >");
+    shortcut_element.addClass("shortcut");
+    shortcut_element.attr("src", "https://i.ytimg.com/vi/" + youtube_id + "/hqdefault.jpg");
+    shortcut_element.attr("youtube_id", youtube_id);
+    shortcut_element.attr("data-toggle", "tooltip");
+    shortcut_element.attr("data-placement", "bottom");
+    shortcut_element.attr("title", "點擊觀看作品介紹");
+    link_element.html(shortcut_element);
+    link_element = link_element.prop('outerHTML');
+
+    caption = caption.replace(patt2, link_element);
+    return caption;
+}
+
 function render_icon(post) {
     var ul = $("<ul class='icons'></ul>");
-    var li_link = $('<li class="link"><a href="' + post['url-with-slug'] + '" title="View Post" class="social-eye"></a></li>');
+    var li_link = $('<li class="link"><a href="' + post.post_url + '" title="View Post" class="social-eye"></a></li>');
     var li_like = $('<li class="like"><div class="like_button" data-post-id="' + post.id + '" data-blog-name="woolito" id="like_button_' + post.id + '"></div></li>');
-    var iframe = $('<iframe id="like_iframe_' + post.id + '" src="http://assets.tumblr.com/assets/html/like_iframe.html?_v=c301660e229be2b44e2e912486577f3c#name=woolito&post_id='+post.id+'&color=black&rk='+post['reblog-key']+'&root_id='+post.id+'" scrolling="no" class="like_toggle" allowtransparency="true" width="16" height="16" frameborder="0" name="like_iframe_'+post.id+'"></iframe>');
+    var iframe = $('<iframe id="like_iframe_' + post.id + '" src="http://assets.tumblr.com/assets/html/like_iframe.html?_v=c301660e229be2b44e2e912486577f3c#name=woolito&post_id='+post.id+'&color=black&rk='+post.reblog_key+'&root_id='+post.id+'" scrolling="no" class="like_toggle" allowtransparency="true" width="16" height="16" frameborder="0" name="like_iframe_'+post.id+'"></iframe>');
     li_like.find(".like_button").append(iframe);
-    var li_reblog = $('<li class="reblog"><i class="reblog-icon"><a targer="_blank" href="https://www.tumblr.com/reblog/' + post.id + '/' + post['reblog-key'] + '" class="reblog_button" style="display:block; width:16px; height:16px;"><svg width="100%" height="100%" viewBox="0 0 21 21" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000"><path d="M5.01092527,5.99908429 L16.0088498,5.99908429 L16.136,9.508 L20.836,4.752 L16.136,0.083 L16.1360004,3.01110845 L2.09985349,3.01110845 C1.50585349,3.01110845 0.979248041,3.44726568 0.979248041,4.45007306 L0.979248041,10.9999998 L3.98376463,8.30993634 L3.98376463,6.89801007 C3.98376463,6.20867902 4.71892527,5.99908429 5.01092527,5.99908429 Z"></path><path d="M17.1420002,13.2800293 C17.1420002,13.5720293 17.022957,14.0490723 16.730957,14.0490723 L4.92919922,14.0490723 L4.92919922,11 L0.5,15.806 L4.92919922,20.5103758 L5.00469971,16.9990234 L18.9700928,16.9990234 C19.5640928,16.9990234 19.9453125,16.4010001 19.9453125,15.8060001 L19.9453125,9.5324707 L17.142,12.203"></path></svg></a></i></li>');
-    var li_share = $('<li class="sharer"><a class="social-export"></a><div class="sharer-wrap"><ul><li class="facebook"><a href="http://www.facebook.com/sharer.php?u=' + post['url-with-slug'] + '" title="Share on Facebook" class="social-facebook" target="_blank" ></a></li><li class="gplus"><a href="https://plus.google.com/share?url=' + post['url-with-slug'] + '" title="Share on Google +" class ="social-gplus" target="_blank"></a></li><li class="pinterest" ><a href="https://pinterest.com/pin/create/button/?url=' + post['url-with-slug'] + '&amp;media=http://67.media.tumblr.com/avatar_fbc2eabcb706_128.png" title="Share on Pinterest" class="social-pinterest" target="_blank"></a></li></ul></div></li>');
+    var li_reblog = $('<li class="reblog"><i class="reblog-icon"><a targer="_blank" href="https://www.tumblr.com/reblog/' + post.id + '/' + post.reblog_key + '" class="reblog_button" style="display:block; width:16px; height:16px;"><svg width="100%" height="100%" viewBox="0 0 21 21" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000"><path d="M5.01092527,5.99908429 L16.0088498,5.99908429 L16.136,9.508 L20.836,4.752 L16.136,0.083 L16.1360004,3.01110845 L2.09985349,3.01110845 C1.50585349,3.01110845 0.979248041,3.44726568 0.979248041,4.45007306 L0.979248041,10.9999998 L3.98376463,8.30993634 L3.98376463,6.89801007 C3.98376463,6.20867902 4.71892527,5.99908429 5.01092527,5.99908429 Z"></path><path d="M17.1420002,13.2800293 C17.1420002,13.5720293 17.022957,14.0490723 16.730957,14.0490723 L4.92919922,14.0490723 L4.92919922,11 L0.5,15.806 L4.92919922,20.5103758 L5.00469971,16.9990234 L18.9700928,16.9990234 C19.5640928,16.9990234 19.9453125,16.4010001 19.9453125,15.8060001 L19.9453125,9.5324707 L17.142,12.203"></path></svg></a></i></li>');
+    var li_share = $('<li class="sharer"><a class="social-export"></a><div class="sharer-wrap"><ul><li class="facebook"><a href="http://www.facebook.com/sharer.php?u=' + post.post_url + '" title="Share on Facebook" class="social-facebook" target="_blank" ></a></li><li class="gplus"><a href="https://plus.google.com/share?url=' + post.post_url + '" title="Share on Google +" class ="social-gplus" target="_blank"></a></li><li class="pinterest" ><a href="https://pinterest.com/pin/create/button/?url=' + post.post_url + '&amp;media=http://67.media.tumblr.com/avatar_fbc2eabcb706_128.png" title="Share on Pinterest" class="social-pinterest" target="_blank"></a></li></ul></div></li>');
     ul.append(li_link).append(li_like).append(li_reblog).append(li_share);
     return ul;
 }
