0a98d8be988dc45822b9cb2cc0dbafaa3d97352f
contemplator <contemplation8213@gmail.com>
2016-08-18 22:26:46 +0800

    完成contact.html 樣式

---
+++ custom/js/contact.js
@@ -0,0 +1,13 @@
+$(function(){
+	$(".name .input-field").click(function(){
+		$("#input-name").focus();
+	});
+
+	$(".phone .input-field").click(function(){
+		$("#input-phone").focus();
+	});
+
+	$(".email .input-field").click(function(){
+		$("#input-email").focus();
+	});
+});
