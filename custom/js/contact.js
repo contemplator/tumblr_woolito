2d3298aa3fb92c6e825355c29b28f0a4dc373c4e
contemplator <contemplation8213@gmail.com>
2016-08-20 22:17:20 +0800

    contact mobile html style finish 1 version

--- custom/js/contact.js
+++ custom/js/contact.js
@@ -1,4 +1,21 @@
 $(function(){
+	console.log("width: " + $(window).width());
+    console.log("user agent: " + navigator.userAgent);
+	if (ismobile()) {
+        location.href = "contact-mobile";
+    } else {
+        if (ismobile_size()) {
+            location.href = "contact-mobile";
+        }
+    }
+
+    $(window).resize(function() {
+        if (ismobile_size()) {
+            location.href = "works-mobile";
+        }
+    });
+
+
 	$(".name .input-field").click(function(){
 		$("#input-name").focus();
 	});
@@ -11,3 +28,26 @@ $(function(){
 		$("#input-email").focus();
 	});
 });
+
+function ismobile() {
+    if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function ismobile_size() {
+    var width = $(window).width();
+    var href = location.href;
+    var index = href.lastIndexOf("/") + 1;
+    href = href.substring(index);
+    // console.log(width + ", " + href);
+    if (width <= 768) {
+        return true;
+        // location.href = "works-mobile.html";
+    } else {
+        return false;
+        // location.href = "works.html";
+    }
+}
